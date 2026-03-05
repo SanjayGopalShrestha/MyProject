d4c42c87d568e003979edcfe4fc07834abf31763
Sanjay Gopal Shrestha <mr.sanjayshrestha@gmail.com>
2026-03-05 18:11:06 +0545

    Update Mail.java

--- Advanced Java Programming/Mail.java
+++ Advanced Java Programming/Mail.java
@@ -14,21 +14,31 @@ import java.util.Properties;
 public class Rough {
     public static void main(String[] args) throws Exception {
 
+        String from = "mr.sanjayshrestha@gmail.com";
+        String password = "kiwh wvif ixbz bccl";  // Must be App Password if 2FA enabled
+        String to = "mr.sanjayshrestha@gmail.com";
+
         Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
 
-        Session session = Session.getInstance(props);
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, password);
+            }
+        });
 
         Message message = new MimeMessage(session);
-        message.setFrom(new InternetAddress("mr.sanjayshrestha@gmail.com"));
+        message.setFrom(new InternetAddress(from));
         message.setRecipients(Message.RecipientType.TO,
-                InternetAddress.parse("mr.sanjayshrestha@gmail.com"));
-        message.setSubject("Test Mail");
-        message.setText("Hello from Java Mail API");
+                InternetAddress.parse(to));
+        message.setSubject("Test Email");
+        message.setText("Hello, this is a test email from JavaMail!");
 
         Transport.send(message);
+
+        System.out.println("Email sent successfully!");
     }
 }
