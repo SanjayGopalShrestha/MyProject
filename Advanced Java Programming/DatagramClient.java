993c6a9f4a5ed7f4f03341df46f113f0e2f4d309
Sanjay Gopal Shrestha <mr.sanjayshrestha@gmail.com>
2026-03-05 07:04:24 +0545

    Update DatagramClient.java

--- Advanced Java Programming/DatagramClient.java
+++ Advanced Java Programming/DatagramClient.java
@@ -1,1 +1,19 @@
+// Sender 
+
+import java.net.*;
+
+public class DatagramClient {
+    public static void main(String[] args) throws Exception {
+        DatagramSocket socket = new DatagramSocket();
+        String msg = "Hello Server";
+        byte[] buffer = msg.getBytes();
+
+        InetAddress address = InetAddress.getByName("localhost");
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 9876);
+
+        socket.send(packet);
+        System.out.println("Message sent");
+        socket.close();
+    }
+}
 
