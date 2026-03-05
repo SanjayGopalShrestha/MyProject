0deed1b82c248d497e8cb45dcf9ca22170ec37c3
Sanjay Gopal Shrestha <mr.sanjayshrestha@gmail.com>
2026-03-05 07:03:19 +0545

    Update DatagramServer.java

--- Advanced Java Programming/DatagramServer.java
+++ Advanced Java Programming/DatagramServer.java
@@ -1,1 +1,19 @@
+// Receiver
+
+import java.net.*;
+
+public class DatagramServer {
+    public static void main(String[] args) throws Exception {
+        DatagramSocket socket = new DatagramSocket(9876);
+        byte[] buffer = new byte[1024];
+
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        socket.receive(packet); // Wait for a packet
+
+        String received = new String(packet.getData(), 0, packet.getLength());
+        System.out.println("Received: " + received);
+
+        socket.close();
+    }
+}
 
