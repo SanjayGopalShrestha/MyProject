fbeb134c102c3bfe973317f2a390138be02120a3
Sanjay Gopal Shrestha <mr.sanjayshrestha@gmail.com>
2026-03-05 07:27:38 +0545

    Update TCPClient.java

--- Advanced Java Programming/TCPClient.java
+++ Advanced Java Programming/TCPClient.java
@@ -1,1 +1,13 @@
+//After connection both can send and receive
+
+import java.net.*;
+
+public class TCPClient {
+    public static void main(String[] args) throws Exception {
+
+        Socket socket = new Socket("localhost", 5000);
+
+        System.out.println("Connected to server");
+    }
+}
 
