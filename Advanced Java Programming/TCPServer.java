a193b4d6371f02b78e73e9adcd54ace2c1629c4c
Sanjay Gopal Shrestha <mr.sanjayshrestha@gmail.com>
2026-03-05 07:24:24 +0545

    Update TCPServer.java

--- Advanced Java Programming/TCPServer.java
+++ Advanced Java Programming/TCPServer.java
@@ -1,1 +1,24 @@
+/*
+In Java TCP socket programming using Transmission Control Protocol, both the client and the server can 
+be senders and receivers.
+
+TCP communication is two-way (full duplex), meaning both sides can send and receive data.
+
+After the connection is established, both can send messages.  
+*/
+  
+import java.net.*;
+import java.io.*;
+
+public class TCPServer {
+    public static void main(String[] args) throws Exception {
+
+        ServerSocket server = new ServerSocket(5000);
+        System.out.println("Server waiting for client...");
+
+        Socket socket = server.accept();   // waits for client
+
+        System.out.println("Client connected: " + socket.getInetAddress());
+    }
+}
 
