3bd7d7ddf882287e74c19d76946001cbc1ab7ab1
Sanjay Gopal Shrestha <mr.sanjayshrestha@gmail.com>
2026-03-07 16:50:29 +0545

    Update Servlet.java

--- Advanced Java Programming/Servlet.java
+++ Advanced Java Programming/Servlet.java
@@ -2,6 +2,22 @@
 
 @WebServlet("/Servlet") and class Servlet have to be same, this is called servlet mapping
 
+- To Run: 
+  Save file in C:\tomcat\apache-tomcat-9.0.113\webapps\YourApp\src\com\example\Rough1.java
+  
+  Compile: 
+  C:\tomcat\apache-tomcat-9.0.113\webapps\YourApp\src\com\example>javac -  
+  cp "C:\tomcat\apache-tomcat-9.0.113\lib\servlet-api.jar" Rough1.java
+ 
+- It will create Rough1.class file in C:\tomcat\apache-tomcat-9.0.113\webapps\YourApp\src\com\example\Rough1.java
+
+- Move that Rough1.class file to C:\tomcat\apache-tomcat-9.0.113\webapps\YourApp\WEB-INF\classes\com\example\Rough1.class
+
+- Now, open a browser, type: http://localhost:8080/YourApp/Rough1
+
+
+
+
 */
 
 
