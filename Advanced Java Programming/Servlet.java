33cf80a11bb82c56d836c389b13e8ae2e890175f
Sanjay Gopal Shrestha <mr.sanjayshrestha@gmail.com>
2026-03-07 17:01:52 +0545

    Refine servlet mapping and file location instructions
    Updated servlet mapping instructions and file paths.

--- Advanced Java Programming/Servlet.java
+++ Advanced Java Programming/Servlet.java
@@ -1,22 +1,36 @@
 /*
-
-@WebServlet("/Servlet") and class Servlet have to be same, this is called servlet mapping
-
 - To Run: 
+  @WebServlet("/Servlet") and class Servlet have to be same i.e. in both places same name "servlet", this is called servlet mapping
+
   Save file in C:\tomcat\apache-tomcat-9.0.113\webapps\YourApp\src\com\example\Rough1.java
   
   Compile: 
-  C:\tomcat\apache-tomcat-9.0.113\webapps\YourApp\src\com\example>javac -  
-  cp "C:\tomcat\apache-tomcat-9.0.113\lib\servlet-api.jar" Rough1.java
+  C:\tomcat\apache-tomcat-9.0.113\webapps\YourApp\src\com\example>javac -cp "C:\tomcat\apache-tomcat-9.0.113\lib\servlet-api.jar" Rough1.java
  
-- It will create Rough1.class file in C:\tomcat\apache-tomcat-9.0.113\webapps\YourApp\src\com\example\Rough1.java
+- It will create Rough1.class file in C:\tomcat\apache-tomcat-9.0.113\webapps\YourApp\src\com\example\Rough1.class
 
 - Move that Rough1.class file to C:\tomcat\apache-tomcat-9.0.113\webapps\YourApp\WEB-INF\classes\com\example\Rough1.class
 
 - Now, open a browser, type: http://localhost:8080/YourApp/Rough1
 
+.java and .class file locations
+===============================
+apache-tomcat-9.0.113/
+ └── webapps/
+      └── YourApp/
+           ├── src/
+               ├── com/
+                   ├── example/  
+                       ── Rough1.java
 
-
+apache-tomcat-9.0.113/
+ └── webapps/
+      └── YourApp/
+           ├── WEB-INF/
+               ├── classes/
+                   ├── com/  
+                       ├── example/ 
+                          ---Rough1.class
 
 */
 
