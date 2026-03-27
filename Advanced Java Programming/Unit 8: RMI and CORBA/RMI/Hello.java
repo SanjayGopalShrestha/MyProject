61625dfcb81d00c33acb996bebb7065023c801da
Sanjay Gopal Shrestha <mr.sanjayshrestha@gmail.com>
2026-03-27 08:04:07 +0545

    Update Hello.java

--- Advanced Java Programming/Unit 8: RMI and CORBA/RMI/Hello.java
+++ Advanced Java Programming/Unit 8: RMI and CORBA/RMI/Hello.java
@@ -1,6 +1,7 @@
 // Create Remote Interface (Hello.java)
 
-import java.rmi.*;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface Hello extends Remote {
     String sayHello() throws RemoteException;
