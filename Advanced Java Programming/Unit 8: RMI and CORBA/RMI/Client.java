b64d1c06c9aaa4dfbffaef6263806fd45eb88da3
Sanjay Gopal Shrestha <mr.sanjayshrestha@gmail.com>
2026-03-27 08:06:59 +0545

    Update Client.java

--- Advanced Java Programming/Unit 8: RMI and CORBA/RMI/Client.java
+++ Advanced Java Programming/Unit 8: RMI and CORBA/RMI/Client.java
@@ -1,6 +1,7 @@
 // 4. Client Code (Client.java)
 
-import java.rmi.registry.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class Client {
     public static void main(String[] args) {
@@ -10,7 +11,7 @@ public class Client {
             Hello stub = (Hello) registry.lookup("HelloService");
 
             String response = stub.sayHello();
-            System.out.println(response);
+            System.out.println("Response from server: " + response);
 
         } catch (Exception e) {
             e.printStackTrace();
